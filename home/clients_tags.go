228cfc54ef05e6538db16c4e746d7589de7c71a4
Simon Zolin <s.zolin@adguard.com>
2020-01-23 13:40:10 +0300

    more tags

--- home/clients_tags.go
+++ home/clients_tags.go
@@ -2,19 +2,23 @@ package home
 
 var clientTags = []string{
 	"device_audio",
+	"device_nas",
 	"device_other",
 	"device_pc",
 	"device_phone",
 	"device_printer",
 	"device_tablet",
+	"device_tv",
 
 	"os_android",
 	"os_ios",
 	"os_linux",
 	"os_macos",
+	"os_other",
 	"os_windows",
 
 	"user_admin",
 	"user_child",
+	"user_other",
 	"user_regular",
 }
